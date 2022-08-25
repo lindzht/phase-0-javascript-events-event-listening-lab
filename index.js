067decf6063ea4327165fd9917ea983b0e3cc69a
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-08-25 19:12:54 -0400

    commit tree 34059fce9b14b30260c0807e7dbb5cc3829b5d39

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+
+
 function addingEventListener() {
+    const input = document.getElementById("button")
+    function clickAlert(){
+        alert(`I was clicked!`);
+    }
+    input.addEventListener("click", clickAlert);
+
 }
+
+addingEventListener();
+
+
+
+
+
+// const body = document.querySelector("body");
+// const fifthChild = body.children[4];
+// const four = fifthChild.getElementsByTagName("div")[2];
+
+// four.addEventListener("click", clickAlert);
+
+// const deepestDivChild = document.querySelector("div div div div div")
+
+
+
+// const deepestDivChild = document.getElementsByTagName("div")[5];
+// deepestDivChild.addEventListener("click", clickAlert);
+
+// main.addEventListener("click", clickAlert);
